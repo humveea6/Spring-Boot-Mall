446f9fa8f7538e5ab67ea32b0c960abe9b8adb28
HumveeA6 <humveea6@163.com>
2020-05-04 21:59:01 +0800

    add order part

---
+++ src/main/java/com/imooc/mall/service/Impl/OrderServiceImpl.java
@@ -0,0 +1,14 @@
+package com.imooc.mall.service.Impl;
+
+import com.imooc.mall.service.IOrderService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author LingChen <lingchen@kuaishou.com>
+ * Created on 2020-05-04
+ */
+@Service
+public class OrderServiceImpl implements IOrderService {
+
+    
+}
